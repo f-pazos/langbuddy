a6bce112b8715666cd49ea7e95e62a8d4d273210
Felipe Pazos <fap26@cornell.edu>
2023-12-26 15:36:33 -0600

    got headful chromium navigating, simple repl, dictionary types

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,61 @@
 use std::error::Error;
+use std::ffi::OsStr;
+use std::{io, fs};
+use std::path::PathBuf;
 use std::time::{SystemTime, self};
 
-use headless_chrome::Browser;
+use headless_chrome::{Browser, LaunchOptions};
 use headless_chrome::protocol::cdp::Page;
+use html_parser::Dom;
+
+
+const WORD_REFERENCE_SP_EN_QUERY: &str = "https://www.wordreference.com/es/en/translation.asp?spen=";
+
+struct WordEntry {
+    word: String, 
+    definition: String,
+    url: String,
+}
+
+struct WordEntries {
+    entries: Vec<WordEntry>,
+}
+
+
+
 
 fn main() -> anyhow::Result<()>{
 
     let last_timestamp = SystemTime::now();
 
-    let browser = Browser::default()?;
-    println!("Browser::default(): {}", last_timestamp.elapsed()?.as_millis());
+    let browser = Browser::new(
+        LaunchOptions::default_builder()
+            .headless(false)
+            .build()
+            .expect("Could not find chrome-executable"),
+    )?;
 
     let tab = browser.new_tab()?;
-    println!("browser.new_tab(): {}", last_timestamp.elapsed()?.as_millis());
-
-    // Navigate to wikipedia
-    let content = tab.navigate_to("https://www.collinsdictionary.com/us/dictionary/spanish-english/casa")?.get_content()?;
-    println!("navigate_to: {}", last_timestamp.elapsed()?.as_millis());
-
-    std::thread::sleep(time::Duration::from_millis(100));
-
-    std::fs::write("out.html", content)?;
 
 
-    Ok(())
+    loop { 
+        println!("input a word to query");
+        let word = input();
+
+        tab.navigate_to(&format!("{}{}", WORD_REFERENCE_SP_EN_QUERY, word))?;
+        tab.wait_until_navigated()?;
+
+        let html = tab.get_content()?;
+        let dom = Dom::parse(&html)?;
+
+
+        std::fs::write("out.html", html)?;
+    }
+}
+
+
+fn input() -> String {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s);
+    return s;
 }
