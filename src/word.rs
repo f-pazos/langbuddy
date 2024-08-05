fe208c79693c1f0d397b514bdce33514c5fbf531
Felipe Pazos <fap26@cornell.edu>
2024-08-05 10:00:04 -0700

    types

---
+++ src/word.rs
@@ -0,0 +1,14 @@
+// pub struct FlashCard {
+//     word: String,
+//     definitions: String,
+// }
+
+// A dictionary has a vocabulary of words.
+// A word is {representation, meaning}
+
+// Some words have definition entries
+// Some words have parts of speech
+
+type Word struct {
+    token: String,
+}
